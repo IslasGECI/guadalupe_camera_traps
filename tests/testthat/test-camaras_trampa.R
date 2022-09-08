3134c48574a7858dc5b79da020c9f68fb9d4f884
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-09-08 15:57:46 -0700

    🚚Move production code to 🇷 folder

--- tests/testthat/test-camaras_trampa.R
+++ tests/testthat/test-camaras_trampa.R
@@ -7,24 +7,6 @@ is_id_consistent_with_zone <- function() {
   is_each_id_consistent_with_zone <- zones_from_id == camaras_trampa$Zona
   return(all(is_each_id_consistent_with_zone))
 }
-get_id_from_outdated_cameras <- function(){
-  obsolete_cameras <- read_csv("../camaras_trampas_guadalupe.csv", show_col_types = FALSE) %>% filter(Vigencia == "obsoleto")
-  obsolete_id <- unique(obsolete_cameras$ID_camara_trampa)
-  return(obsolete_id)
-}
-
-get_cameras_id_after_date <- function(){
-  camaras_trampa$fecha <- lubridate::dmy(camaras_trampa$Fecha_envio_datos)
-  vigentes <- camaras_trampa %>% filter(fecha > lubridate::dmy("16/Jun/2020"))
-  id_vigentes <- unique(vigentes$ID_camara_trampa)
-  return(id_vigentes)
-}
-
-is_obsoleta_in_vigentes <- function(){
-  vigentes <- get_cameras_id_after_date()
-  obsoletas <- get_id_from_outdated_cameras()
-  return(sum(obsoletas %in% vigentes))
-}
 
 
 test_that("ID is consisitent with zone", {

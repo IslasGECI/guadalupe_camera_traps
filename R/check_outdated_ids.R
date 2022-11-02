c7767e77d0e819b4da89bcc35b8341a368c3c992
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-11-02 11:34:59 -0700

    🎨 Format with Styler

--- R/check_outdated_ids.R
+++ R/check_outdated_ids.R
@@ -1,19 +1,19 @@
 camaras_trampa_campo <- read.csv("tests/data/subset_revision_campo.csv")
 
-get_id_from_outdated_cameras <- function(){
+get_id_from_outdated_cameras <- function() {
   obsolete_cameras <- read_csv("../camaras_trampa_campos_guadalupe.csv", show_col_types = FALSE) %>% filter(Vigencia == "obsoleto")
   obsolete_id <- unique(obsolete_cameras$ID_camara_trampa)
   return(obsolete_id)
 }
 
-get_cameras_id_after_date <- function(){
+get_cameras_id_after_date <- function() {
   camaras_trampa_campo$fecha <- lubridate::dmy(camaras_trampa_campo$Fecha_envio_datos)
   vigentes <- camaras_trampa_campo %>% filter(fecha > lubridate::dmy("16/Jun/2020"))
   id_vigentes <- unique(vigentes$ID_camara_trampa)
   return(id_vigentes)
 }
 
-is_obsoleta_in_vigentes <- function(){
+is_obsoleta_in_vigentes <- function() {
   vigentes <- get_cameras_id_after_date()
   obsoletas <- get_id_from_outdated_cameras()
   return(sum(obsoletas %in% vigentes))
